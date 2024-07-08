bffbd4990248ea8bb12e4fd069b5fe67a0b75a2b
Swift <swiftshineexists@gmail.com>
2024-07-08 13:59:05 +0100

    forgot this file

--- src/gfl/string/fixedstring.cpp
+++ src/gfl/string/fixedstring.cpp
@@ -5,39 +5,39 @@
 
 // Fixed String
 
-gfl::FixedString(const char* src) {
+gfl::FixedString::FixedString(const char* src) {
     gfl::mem::Memcpy(this->string, STRING_SIZE, const_cast<char*>(src));
     this->len = strlen(this->string);
 }
 
-gfl::FixedString(BasicString* src) {
+gfl::FixedString::FixedString(BasicString* src) {
     char* s = GFL_BASIC_STRING_CHECK_USE_CHARS(src) ? (char*)src + 1 : src->string;
     gfl::mem::Memcpy(this->string, STRING_SIZE, s);
     this->len = strlen(this->string);
 }
 
-void gfl::FixedReset() {
+void gfl::FixedString::Reset() {
     memset(string, 0, STRING_SIZE);
     len = 0;
 }
 
-gfl::Fixed~FixedString() {
+gfl::FixedString::~FixedString() {
     // gfl::mem::Remove(fixedString, heapID);
 }
 
 
-void gfl::Fixedoperator=(const char* src) {
+void gfl::FixedString::operator=(const char* src) {
     gfl::mem::Memcpy(this->string, STRING_SIZE, const_cast<char*>(src));
     this->len = strlen(this->string);
 }
 
-void gfl::Fixedoperator=(gfl::BasicString* src) {
+void gfl::FixedString::operator=(gfl::BasicString* src) {
     char* s = GFL_BASIC_STRING_CHECK_USE_CHARS(src) ? (char*)src + 1 : src->string;
     gfl::mem::Memcpy(this->string, STRING_SIZE, s);
     this->len = strlen(this->string);
 }
 
-bool gfl::FixedHasForwardSlash() {
+bool gfl::FixedString::HasForwardSlash() {
     for (int i = 0; i < len; i++) {
         if (string[i] == '/') return true;
     }
