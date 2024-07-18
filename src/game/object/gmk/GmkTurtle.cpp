0b56eb20b3606ef5bb31ca130cdca439827d686d
Swift <swiftshineexists@gmail.com>
2024-07-19 00:34:20 +0100

    establish that gimmicks do not get headers

--- src/game/object/gmk/GmkTurtle.cpp
+++ src/game/object/gmk/GmkTurtle.cpp
@@ -1,4 +1,50 @@
-#include <game/object/gmk/GmkTurtle.h>
+#include <game/object/Gimmick.h>
+#include <game/object/PlayerBase.h>
+
+// size: 0x15C
+class GmkTurtle : public Gimmick {
+public:
+    enum State {
+        State_0,
+        MoveLeft,
+        TurnRight,
+        State_3,
+        MoveRight,
+        TurnLeft,
+        State_6,
+        State_7,
+        State_8,
+    };
+
+public:
+    GmkTurtle(StageTask* stageTask);
+    ~GmkTurtle();
+
+    virtual void Update();
+
+    virtual int OnPlayerWeight(PlayerBase*);
+
+    virtual void fn_804FB1D4();
+    void fn_804FB1CC();
+
+public:
+    u32 counterDefaultValue;
+    f32 speed;
+    f32 _138;
+    bool _13C;
+    bool _13D;
+    u16 _13E;
+    u32 counter;
+    u32 numTurtles;
+    u32 currentState;
+    Gimmick* water;
+    void* animnCtrl; // NwAnmCtrl*
+    void* colObjTrans; // ColObjTrans*
+    void* rideHitCtrlTrans; // FlfRideHitCtrlTrans*
+};
+
+
+
 
 GmkTurtle::GmkTurtle(StageTask* stageTask)
     : Gimmick(stageTask, "GmkTurtle")
