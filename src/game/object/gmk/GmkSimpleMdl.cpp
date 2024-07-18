e40d7074b6c7d4afc95c5b8d3cd3f2e8bb1c1de5
Swift <swiftshineexists@gmail.com>
2024-07-19 00:41:53 +0100

    GmkSimpleMdl scaffold

---
+++ src/game/object/gmk/GmkSimpleMdl.cpp
@@ -0,0 +1,26 @@
+#include <game/object/Gimmick.h>
+#include <gfl/file/resfileinfo.h>
+
+namespace gfl { class ScnMdlWrapper; }
+class NwAnm;
+
+// size: 0x158
+class GmkSimpleMdl : public Gimmick {
+public:
+    GmkSimpleMdl();
+    virtual ~GmkSimpleMdl();
+
+    virtual void fn_8000BC7C() override; // (this, PlayerBase*)
+    virtual void fn_8000BC74() override; // (this, undefined4, BasicString*)
+public:
+    gfl::ScnMdlWrapper* modelWrapper;
+    NwAnm* anim1;
+    NwAnm* anim2;
+    NwAnm* anim3;
+    Vec3f _140;
+    u32 _14C;
+    gfl::ResArchivedFileInfo* fileInfo;
+    u32 _154;
+};
+
+ASSERT_SIZE(GmkSimpleMdl, 0x158)
