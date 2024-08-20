32de814021929d386f315e1a370289547134f531
Swift <swiftshineexists@gmail.com>
2024-08-19 22:25:24 -0500

    library move

--- old/src/homebutton/FrameController.cpp
+++
@@ -1,1 +0,0 @@
-#include <homebutton/FrameController.h>
--- old/src/revolution/OS/OSLink.c
+++
@@ -1,6 +0,0 @@
-#include <revolution/OS.h>
-
-void __OSModuleInit(void) {
-    OS_FIRST_REL = OS_LAST_REL = NULL;
-    OS_REL_NAME_TABLE = NULL;
-}
