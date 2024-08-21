ae161e927f8040c4ec426964dcd5a0f5ba483151
Swift <swiftshineexists@gmail.com>
2024-08-20 19:55:43 -0500

    accidentally deleted nw4r headers

---
+++ src/nw4r/snd/snd_SoundStartable.h
@@ -0,0 +1,48 @@
+#ifndef NW4R_SND_SOUND_STARTABLE_H
+#define NW4R_SND_SOUND_STARTABLE_H
+#include "snd_BasicSound.h"
+#include "types_nw4r.h"
+
+namespace nw4r
+{
+    namespace snd
+    {
+        struct SoundStartable
+        {
+            struct StartInfo
+            {
+                
+            };
+            
+            inline SoundStartable() {}
+            inline virtual ~SoundStartable() {} // at 0x8
+            virtual u32 detail_SetupSound(SoundHandle *,
+                u32,
+                detail::BasicSound::AmbientArgInfo *,
+                detail::ExternalSoundPlayer *,
+                bool,
+                const StartInfo *) = 0; // at 0xc
+            virtual u32 detail_ConvertLabelStringToSoundId(const char *) = 0; // at 0x10
+
+            u32 detail_StartSound(SoundHandle *,
+                u32,
+                detail::BasicSound::AmbientArgInfo *,
+                detail::ExternalSoundPlayer *,
+                const StartInfo *);
+
+            u32 detail_HoldSound(SoundHandle *,
+                u32,
+                detail::BasicSound::AmbientArgInfo *,
+                detail::ExternalSoundPlayer *,
+                const StartInfo *);
+
+            u32 detail_PrepareSound(SoundHandle *,
+                u32,
+                detail::BasicSound::AmbientArgInfo *,
+                detail::ExternalSoundPlayer *,
+                const StartInfo *);
+        };
+    }
+}
+
+#endif
