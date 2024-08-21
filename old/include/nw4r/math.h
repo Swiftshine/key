859e7de0c29db116d7fc9704be0ae31a0c51f6e6
Swift <swiftshineexists@gmail.com>
2024-08-20 19:22:39 -0500

    removals

--- old/include/nw4r/math.h
+++
@@ -1,9 +0,0 @@
-#ifndef NW4R_PUBLIC_MATH_H
-#define NW4R_PUBLIC_MATH_H
-
-#include <nw4r/math/math_arithmetic.h>
-#include <nw4r/math/math_geometry.h>
-#include <nw4r/math/math_triangular.h>
-#include <nw4r/math/math_types.h>
-
-#endif
