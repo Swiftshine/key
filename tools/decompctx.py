e24401f44b0409e8c6dcba390ccc9210cf3e956c
Swift <swiftshineexists@gmail.com>
2024-08-24 16:24:05 -0500

    fix include headers for context generation script

--- tools/decompctx.py
+++ tools/decompctx.py
@@ -19,13 +19,19 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = os.path.abspath(os.path.join(script_dir, ".."))
 src_dir = os.path.join(root_dir, "src")
 include_dirs = [
-    os.path.join(root_dir, "include"),
-    os.path.join(root_dir, "include/MSL/"),
-    os.path.join(root_dir, "include/nw4r/"),
-    os.path.join(root_dir, "include/nw4r/math/"),
-    os.path.join(root_dir, "include/nw4r/g3d/"),
-    os.path.join(root_dir, "include/nw4r/ut/"),
-    os.path.join(root_dir, "include/revolution/"),
+    os.path.join(root_dir, "src"),
+    os.path.join(root_dir, "src/fluff/"),
+    os.path.join(root_dir, "src/gfl/"),
+    os.path.join(root_dir, "src/PowerPC_EABI_Support/"),
+    os.path.join(root_dir, "src/PowerPC_EABI_Support/MSL/"),
+    os.path.join(root_dir, "src/PowerPC_EABI_Support/MSL/MSL_C/"),
+    os.path.join(root_dir, "src/PowerPC_EABI_Support/MSL/MSL_C/MSL_Common/"),
+    os.path.join(root_dir, "src/PowerPC_EABI_Support/MSL/MSL_C++/"),
+    os.path.join(root_dir, "src/nw4r/"),
+    os.path.join(root_dir, "src/nw4r/math/"),
+    os.path.join(root_dir, "src/nw4r/g3d/"),
+    os.path.join(root_dir, "src/nw4r/ut/"),
+    os.path.join(root_dir, "src/revolution/"),
     # Add additional include directories here
 ]
 
