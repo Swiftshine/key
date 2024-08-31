d87c009425d16856c17cf635ecbe6ce319aa0dd7
Swift <swiftshineexists@gmail.com>
2024-08-30 20:37:44 -0500

    gfl::ResFileInfo funcs

--- src/gfl/gflResFileInfo_new.h
+++ src/gfl/gflResFileInfo_new.h
@@ -1,11 +1,81 @@
 // add header guards later
 
 #include "gflResInfo.h"
+#include "gflFixedString.h"
+#include "gflFileSystemWii.h"
+
+#include <utility>
 
 namespace gfl {
     class ResFileInfo : public ResInfo {
     public:
         
+        /**
+         * @note Address: 0x8063E364
+         * @note Size: 0x20
+         */
+        ResFileInfo();
+        /**
+         * @note Address: 0x8063E388
+         * @note Size: 0x4
+         */
+        DECL_WEAK ~ResFileInfo();
+
+        /**
+         * @note Address: 0x8063CA00
+         * @note Size: 0x200
+         * @param path This path can be structured as if the target was either a folder or an archive.
+         * A distinction is made between folder and archive when returning the file info.
+         */
+        static ResFileInfo* GetFromPath(const char* path, bool isFolder);
+
+        /**
+         * @note Address: 0x8063D284
+         * @note Size: 0x438
+         * @param path This path is structured as if the archive was a folder.
+         */
+        static bool IsInArchive(const char* path);
+
+        /**
+         * @note Address: 0x8063D6C0
+         * @note Size: 0x1E4
+         * @note This configures a pre-existing `gfl::ResFileInfo` object.
+         */
+        static void Configure(ResFileInfo* dest, const char* path, int arg2, int* arg3, FixedString* str);
+
+
+        /**
+         * @note Address: 0x8063DCD8
+         * @note Size: 0x240
+         * @note This configures a pre-existing `gfl::ResFileInfo` object.
+         * @param fs The fact that this function is only called during the game init process may have something to do with this.
+         * @param path This path can be structured as if the target was either a folder or an archive.
+         * A distinction is made between folder and archive when configuring the file info.
+         */        
+        static void Configure(FileSystemWii* fs, ResFileInfo* dest, const char* path);
+
+        /**
+         * @note Address: 0x8063DF1C
+         * @note Size: 0x448
+         * @note This configures a pre-existing `gfl::ResFileInfo` object.
+         * @param name This value is an `std::pair<bool, const char*>`, where `first` indicates
+         * whether the path is a folder (`true`) or an archive (`false`), and `second` is the path itself.
+         * @param directory The directory of the requested folder or archive.
+         */
+        static void Configure(ResFileInfo* dest, std::pair<bool, const char*>& name, uint checksum, const char* directory);
+    private:
+        /**
+         * @note Address: 0x8063CC04
+         * @note Size: 0x484
+         */
+        static ResFileInfo* GetChildFromPath(const char* path, bool isFolder);
+
+        /**
+         * @note Address: 0x8063D088
+         * @note Size: 0x1F8
+         */
+        static ResFileInfo* CreateFromPath(const char* path, bool isFolder);
+
     };
 
 
@@ -46,6 +116,7 @@ namespace gfl {
     private:
 
         static ResArchivedFileInfo* CreateChild(ResArchivedFileInfo* parent, char* filename, bool useFolder);
+        void* GetData();
     public:
         class DVDDir* mDVDDir;
         uint m_C;
