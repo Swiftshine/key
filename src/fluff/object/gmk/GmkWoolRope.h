b0cc975f577c13cf6de5532b0e0b0bb84f9cca1e
Swift <swiftshineexists@gmail.com>
2024-09-30 20:55:51 -0500

    GmkWoolRope header

---
+++ src/fluff/object/gmk/GmkWoolRope.h
@@ -0,0 +1,67 @@
+#ifndef FLUFF_GMKWOOLROPE_H
+#define FLUFF_GMKWOOLROPE_H
+
+#include "object/wool/WoolLinkObj.h"
+#include "gfl/gflResFileInfo.h"
+#include "graphics/FlfMdlDraw.h"
+#include "object/Gimmick.h"
+
+class GmkWoolHook;
+
+// size: 0x160
+class GmkWoolRope : public Gimmick {
+public:
+    ENUM_CLASS(Color,
+        Brown = 0,
+        Pink = 1,
+        White = 2,
+        Red = 3,
+        Orange = 4,
+        Yellow = 5,
+        LightBlue = 6,
+        RedWhiteBlue = 7,
+        LightGreen = 8,
+        Black = 9,
+        Tan = 10,
+        Cyan = 11,
+        Lavender = 12,
+        Silver = 13,
+
+        GreenCoil = 50,
+        PinkZipper = 51,
+    );
+public:
+    GmkWoolRope(GmkWoolHook* firstHook, GmkWoolHook* secondHook, int woolColorIndex, uint arg4, uint arg5, const char* taskName);
+    virtual ~GmkWoolRope();
+
+    /* FlfGameObj */
+    virtual gfl::Vec3 GetScreenPos() override;
+
+    /* Gimmick */
+    virtual void Update() override;
+
+    /* GmkWoolRope */
+    virtual void LoadTextures();
+    virtual void vfC8();
+    virtual void vfCC();
+    virtual void SetHooks(GmkWoolHook* first, GmkWoolHook* second);
+    virtual void vfD4();
+    virtual void vfD8();
+private:
+    uint m_130;
+    uint m_134;
+    uint m_138;
+    uint m_13C;
+    uint m_140;
+    WoolLinkObj* mWoolLink;
+    int mWoolColorIndex;
+    uint m_14C;
+    GmkWoolHook* mFirstWoolHook;
+    GmkWoolHook* mSecondWoolHook;
+    uint m_158;
+    gfl::ResFileInfo* mResFileInfo;
+};
+
+// ASSERT_SIZE(GmkWoolRope, 0x160);
+
+#endif
