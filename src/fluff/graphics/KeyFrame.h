f18df27babf8ee7a267cb53266141ed8a207d426
Swift <swiftshineexists@gmail.com>
2024-10-12 09:55:29 -0500

    SpringBase header

---
+++ src/fluff/graphics/KeyFrame.h
@@ -0,0 +1,27 @@
+#ifndef FLUFF_KEYFRAME_H
+#define FLUFF_KEYFRAME_H
+
+#include "types.h"
+
+template <typename T>
+class KeyFrame {
+public:
+    inline KeyFrame() {
+        m_4 = nullptr;
+        m_8 = nullptr;
+        m_C = nullptr;
+        mFirst = 0.0f;
+        mSecond = 0.0f;
+    }
+
+    virtual ~KeyFrame();
+private:
+    void* m_4;
+    void* m_8;
+    void* m_C;
+    // these fields are guessed
+    T mFirst;
+    T mSecond;
+};
+
+#endif
--- src/fluff/object/SpringBase.h
+++ src/fluff/object/SpringBase.h
@@ -2,9 +2,21 @@
 #define FLUFF_SPRINGBASE_H
 
 #include "object/FlfGameObj.h"
+#include "graphics/KeyFrame.h"
+#include "gfl/gflArray.h"
 
 // size: 0x150
 class SpringBase : public FlfGameObj {
+public:
+    class Spring {
+
+    };
+
+    class Particle {
+    public:
+        Particle();
+        ~Particle();
+    };
 public:
     SpringBase(u32, const char* taskName);
     virtual ~SpringBase();
@@ -18,7 +30,38 @@ public:
     virtual void vf78();
     virtual void vf7C();
 private:
-    STRUCT_FILL(0x150 - sizeof(FlfGameObj));
+    gfl::Task mTask;
+    int m_98;
+    int m_9C;
+    gfl::Array<Particle> mParticles;
+    int m_A4;
+    int m_A8;
+    int m_AC;
+    int m_B0;
+    int m_B4;
+    KeyFrame<float> mKeyFrame1;
+    bool m_D0;
+    KeyFrame<float> mKeyFrame2;
+    bool m_EC;
+    KeyFrame<float> mKeyFrame3;
+    bool m_108;
+    int m_10C;
+    float m_110;
+    float m_114;
+    float m_118;
+    float m_11C;
+    float m_120;
+    float m_124;
+    float m_128;
+    float m_12C;
+    float m_130;
+    float m_134;
+    float m_138;
+    float m_13C;
+    float m_140;
+    int m_144;
+    int m_148;
+    int m_148;
 };
 
 ASSERT_SIZE(SpringBase, 0x150);
