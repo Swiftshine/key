0bd446deb127371cb9e2d9fe287296eec12d1739
Swift <swiftshineexists@gmail.com>
2024-10-16 08:51:50 -0500

    Language header

---
+++ src/fluff/language/Language.h
@@ -0,0 +1,55 @@
+#ifndef FLUFF_LANGUAGE_H
+#define FLUFF_LANGUAGE_H
+
+#include "types.h"
+#include <revolution/SC.h>
+
+namespace Language {
+    ENUM_CLASS(RegionType,
+        Japan = 0,
+        US = 1,
+        Europe = 2,
+        // Korea = 3?
+    );
+
+    ENUM_CLASS(RegionCode,
+        Japan = 'FLJP',
+        US = 'FLUS',
+        Europe = 'FLEU',
+        // Korea = 'FLKR'?
+    );
+
+    ENUM_CLASS(LanguageType,
+        JP_Japanese = 0,
+
+        US_English = 1,
+        US_French = 2,
+        US_Spanish = 3,
+
+        EU_English = 4,
+        EU_German = 5,
+        EU_French = 6,
+        EU_Spanish = 7,
+        EU_Italian = 8,
+        
+        // everything below this point is assumed based on the SCLanguage enum
+
+        EU_Dutch = 9,
+        CH_ChineseSimplified = 10,
+        CH_ChineseTraditional = 11,
+        KR_Korean = 12,
+    );
+
+    static int CurrentRegionType;
+    static int CurrentRegionCode;
+    static int CurrentLanguageType;
+
+    void SetCurrentRegionType(int type);
+    int GetCurrentRegionType();
+    uint GetCurrentRegionCode(int type);
+    uint GetRegionCodeByRegionType(int type);
+    int GetLanguageType(int regionType, SCLanguage language);
+    void SetCurrentLanguageType(int type);
+}
+
+#endif
