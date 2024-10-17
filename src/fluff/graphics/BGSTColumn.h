57f090194f26554e662d53ffa1c6ad087ef15009
Swift <swiftshineexists@gmail.com>
2024-10-16 20:06:54 -0500

    fix fields, add type

--- src/fluff/graphics/BGSTColumn.h
+++ src/fluff/graphics/BGSTColumn.h
@@ -1,17 +1,26 @@
 #ifndef FLUFF_BGSTCOLUMN_H
 #define FLUFF_BGSTCOLUMN_H
 
+#include "types.h"
+
 namespace BGST {
 
     struct Column {
-    
-        unsigned short m_0;
-        u32 m_4;
-        short m_8;
-        unsigned short mType;
-        unsigned short mImageIndex;
-        unsigned short mShadowImageIndex;
+        ENUM_CLASS(Type,
+            Shadowless = -2u,
+        );
+
+        u16 m_0;
+        u16 m_2;
+        u16 m_4;
+        u16 m_6;
+        u16 m_8;
+        u16 mType;
+        u16 mImageIndex;
+        u16 mShadowImageIndex;
     };
+
+    ASSERT_SIZE(Column, 0x10);
 }
 
 #endif
