f8ece4e45fd6c7ec11c81d0ef9476017905b5fe8
Swift <swiftshineexists@gmail.com>
2024-11-03 15:29:42 -0600

    add CollisionEntry header

---
+++ src/fluff/object/collision/CollisionEntry.h
@@ -0,0 +1,43 @@
+#ifndef FLUFF_COLLISIONENTRY_H
+#define FLUFF_COLLISIONENTRY_H
+
+#include "object/collision/CollisionInfo.h"
+#include "object/collision/ObjHit.h"
+
+struct CollisionBounds2D {
+    nw4r::math::VEC2 mBoundsStart;
+    nw4r::math::VEC2 mBoundsEnd;
+    nw4r::math::VEC2 mMatrixDifference;
+};
+
+struct CollisionBounds3D {
+    nw4r::math::VEC3 mBoundsStart;
+    nw4r::math::VEC2 mMatrixDifference;
+};
+
+class CollisionEntry {
+public:
+    CollisionEntry();
+    DECL_WEAK ~CollisionEntry();
+    
+    static CollisionEntry* Get(CollisionTemplate* collisionTemplate, IObjHitCB* callbacks, FlfGameObj* owner, nw4r::math::MTX34& matrix, const char* name);
+    static void Remove(CollisionEntry* entry);
+    void Copy(CollisionTemplate* collisionTemplate, IObjHitCB* callbacks, FlfGameObj* owner, nw4r::math::MTX34& matrix);
+    void Reset();
+    static void CopyDimensions(nw4r::math::VEC2& dest, CollisionEntry* entry);
+    void SetDimensions(float& x, float& y);
+    static void GetMatrixDifference(nw4r::math::VEC2& dest, CollisionEntry* entry);
+    int UpdateOwner();
+    CollisionBounds3D* Get3DBounds();
+    CollisionBounds2D* Get2DBounds();
+    void ValidateBounds(int arg1, nw4r::math::VEC2& arg2);
+
+public:
+    CollisionInfo mInfo;
+    CollisionBounds2D m2DBounds;
+    CollisionBounds3D m3DBounds;
+};
+
+ASSERT_SIZE(CollisionEntry, 0xB4);
+
+#endif
---
+++ src/fluff/object/collision/ObjHit.h
@@ -0,0 +1,15 @@
+#ifndef FLUFF_OBJHIT_H
+#define FLUFF_OBJHIT_H
+
+class CollisionEntry;
+class IObjHitCB;
+class FlfGameObj;
+struct CollisionTemplate;
+#include <nw4r/math.h>
+
+namespace ObjHit {
+
+    CollisionEntry* GetEntry(CollisionTemplate* collisionTemplate, IObjHitCB* callbacks, FlfGameObj* owner, nw4r::math::MTX34* matrix, const char* name);
+}
+
+#endif
