525f1f6aa99646d7bd1e111e09ecbceaf7c5a598
Swift <swiftshineexists@gmail.com>
2024-11-10 10:32:27 -0600

    finish FullSortScene header

---
+++ src/fluff/graphics/FullSortGroup.h
@@ -0,0 +1,6 @@
+#ifndef FLUFF_FULLSORTGROUP_H
+#define FLUFF_FULLSORTGROUP_H
+
+class FullSortGroup;
+
+#endif
--- src/fluff/graphics/FullSortScene.h
+++ src/fluff/graphics/FullSortScene.h
@@ -1,12 +1,27 @@
 #ifndef FLUFF_FULLSORTSCENE_H
 #define FLUFF_FULLSORTSCENE_H
 
+#include "types.h"
 #include "gfl/gflScene.h"
 #include "gfl/gflScnMdlWrapper.h"
+#include "graphics/FullSortGroup.h"
 
+// size: 0x34
 class FullSortScene : public gfl::Scene {
 public:
-    void SetModelWrapper(gfl::ScnMdlWrapper* modelWrapper);
+    FullSortScene(u8 heapID, int arg2, int arg3);
+    virtual ~FullSortScene();
+
+    /* gfl::Scene */
+    virtual void Insert(nw4r::g3d::ScnObj* object);
+    virtual void Remove(nw4r::g3d::ScnObj* object);
+    virtual void DrawOpa();
+    virtual void DrawXlu();
+    virtual bool IsScnRootSizeValid();
+private:
+    FullSortGroup* mFullSortGroup;
 };
+
+ASSERT_SIZE(FullSortScene, 0x34);
 
 #endif
