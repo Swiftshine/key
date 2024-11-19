ff90a6471bd68b6c2007938fdaa6c46ac72a0999
Swift <swiftshineexists@gmail.com>
2024-11-18 18:00:42 -0600

    env::EnvObject header

---
+++ src/fluff/env/EnvObject.h
@@ -0,0 +1,31 @@
+#ifndef FLUFF_ENVOBJECT_H
+#define FLUFF_ENVOBJECT_H
+
+#include "gfl/gflTask.h"
+
+namespace env {
+    class EnvObject {
+    public:
+        EnvObject(gfl::Task* parentTask, const char* taskName, uint flagIndex);
+        EnvObject(gfl::Task* parentTask, u8 taskFlags, const char* taskName, uint flagIndex);
+        virtual ~EnvObject();
+
+        // called via functor class method
+        void DoUpdate();
+        gfl::Task* GetNewTask(gfl::Task* parentTask, u8 taskFlags, const char* taskName, uint arg4);
+        // resets the flags and applies the given flags
+        void SetTaskFlags(uint flags);
+        // applies the given flags
+        void ApplyTaskFlags(uint flags);
+        void SetTaskFlagsByFlagIndex(uint index);
+        
+        void fn_8005E718(uint arg1, uint arg2);
+        virtual void Update() = 0;
+    private:
+        gfl::Task* mTask;
+        uint mTaskFlags;
+    };
+}
+
+
+#endif
