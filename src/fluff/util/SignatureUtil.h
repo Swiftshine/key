2693e31fa43f7436eaac281ddac561b5a09e3f5a
Swift <swiftshineexists@gmail.com>
2024-11-21 19:32:37 -0600

    document signature types

--- src/fluff/util/SignatureUtil.h
+++ src/fluff/util/SignatureUtil.h
@@ -10,4 +10,11 @@ namespace SignatureUtil {
     void GetSignature(uint src, char* dst1, char* dst2, char* dst3, char* dst4);
 }
 
+// listed are the different signature types seen in structures related to game work
+// A___ = Area [number]
+// E___ = Environment [number]
+// SN__ = Stage (Normal) [number]
+// SB__ = Stage (Boss) [number]
+// C___ = ?
+
 #endif
