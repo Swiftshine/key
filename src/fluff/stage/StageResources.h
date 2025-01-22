8b7c7fa4b88fb897a7e420cb8749b38c9492853a
Swift <swiftshineexists@gmail.com>
2025-01-22 13:32:59 -0600

    map out StageResources functions

--- src/fluff/stage/StageResources.h
+++ src/fluff/stage/StageResources.h
@@ -5,6 +5,7 @@
 #include "gfl/gflLinkedList.h"
 #include <string>
 #include "object/Gimmick.h"
+#include "mapdata/Mapdata.h"
 
 class StageResources {
 private:
@@ -17,8 +18,18 @@ public:
         return sInstance;
     }
 
-    void AddGimmickName(const char*);
-    void AddResourceName(const char*);
+    void RegisterMapdataResources(Mapdata* mapdata);
+    void AddResourceName(const char* name);
+    void AddGimmickName(const char* name);
+    void AddCharaResourceName(const char* name);
+    void AddGimmickBuildInfo(Gimmick::GimmickBuildInfo* buildInfo);
+    void AddResourceNameByGimmickID(int id);
+    void fn_80052228();
+    void fn_800522C4(uint index);
+    void fn_8005234C(uint arg1);
+    void fn_800524F8(const char* name, bool arg2);
+    void fn_80052560(const char* name, bool arg2);
+    void fn_800525C8(const char* name, bool arg2);
 private:
     gfl::LinkedList<std::string*> mGimmickResourceNameList;
     gfl::LinkedList<std::string*> mGimmickNameList;
