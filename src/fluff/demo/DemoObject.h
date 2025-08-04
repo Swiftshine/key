97fce94c99ebd900db98fdd1cba0bae19e3b88bc
Swift <swiftshineexists@gmail.com>
2025-08-04 10:54:27 -0500

    demo/DemoObject

--- src/fluff/demo/DemoObject.h
+++ src/fluff/demo/DemoObject.h
@@ -3,14 +3,15 @@
 
 #include "gfl/gflTask.h"
 #include "gfl/gflPointer.h"
+
 namespace demo {
     class DemoObject {
     public:
-        DemoObject(gfl::Task* parentTask, const char* taskName, uint index);
+        DemoObject(gfl::Task* pParentTask, const char* pTaskName, uint index);
         virtual ~DemoObject();
         
         void fn_802A4F28();
-        gfl::Task* InitTask(gfl::Task* parent, u8 flags, const char* taskName, uint arg4) DONT_INLINE_CLASS;
+        gfl::Task* InitTask(gfl::Task* pParentTask, u8 flags, const char* pTaskName, uint arg4) DONT_INLINE_CLASS;
     private:
         gfl::Pointer<gfl::Task> mTask;
         uint m_8;
