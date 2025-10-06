564e1cae58a9cb199b09d47476c60a341965ae4b
Swift <swiftshineexists@gmail.com>
2025-10-06 16:09:06 -0500

    object/callback

--- src/fluff/object/callback/CB_PlayerBase.h
+++ src/fluff/object/callback/CB_PlayerBase.h
@@ -5,7 +5,10 @@
 
 class CB_PlayerBase : public nw4r::g3d::IScnObjCallback {
 public:
-    DECL_WEAK virtual ~CB_PlayerBase();
+
+    /* Virtual Methods */
+    
+    /* 0x08 */ DECL_WEAK virtual ~CB_PlayerBase();
 };
 
 #endif
