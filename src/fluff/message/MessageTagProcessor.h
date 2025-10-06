2ccd592a1478df323f924c6413b7d221c473df47
Swift <swiftshineexists@gmail.com>
2025-10-06 16:07:03 -0500

    MessageTagProcessor.h

--- src/fluff/message/MessageTagProcessor.h
+++ src/fluff/message/MessageTagProcessor.h
@@ -4,19 +4,29 @@
 #include "types.h"
 
 namespace message {
-    class MessageTagProcessor {
-    public:
-        MessageTagProcessor();
-        virtual ~MessageTagProcessor();
-    private:
-        float m_4;
-        u8 m_8;
-        u8 m_9;
-        u8 m_A;
-        u8 m_B;
-    };
 
-    ASSERT_SIZE(MessageTagProcessor, 0xC);
+/// @note Size: `0xC`
+class MessageTagProcessor {
+public:
+    /* Constructor */
+
+    MessageTagProcessor();
+
+    /* Virtual Methods */
+    
+    /* 0x08 */ virtual ~MessageTagProcessor();
+
+    /* Class Members */
+
+    /* 0x4 */ float m_4;
+    /* 0x8 */ u8 m_8;
+    /* 0x9 */ u8 m_9;
+    /* 0xA */ u8 m_A;
+    /* 0xB */ u8 m_B;
+};
+
+ASSERT_SIZE(MessageTagProcessor, 0xC);
+
 }
 
 #endif
