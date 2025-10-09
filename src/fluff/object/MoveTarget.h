c5f7e738606da635b2efce295345138be0ee46b1
Swift <swiftshineexists@gmail.com>
2025-10-09 18:31:25 -0500

    MoveTarget

--- src/fluff/object/MoveTarget.h
+++ src/fluff/object/MoveTarget.h
@@ -3,13 +3,23 @@
 
 #include "types.h"
 
+/// @note Size: `0x48`
 class MoveTarget {
-private:
-    float m_0[17];
 public:
-    virtual ~MoveTarget();
+    /* Class Members */
+
+    /* 0x0 */ float m_0[17];
+public:
+    /* Constructor */
+
+    MoveTarget();
+
+    /* Virtual Methods */
+
+    /* 0x08 */ virtual ~MoveTarget();
+    /* 0x0C */ virtual void vfC(float*);
+    /* 0x10 */ virtual void vf10(float*);
 };
 
-ASSERT_SIZE(MoveTarget, 0x48);
 
 #endif
