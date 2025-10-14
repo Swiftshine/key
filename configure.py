c68cc1e682d59a8895d9b76483a4a81c3b471050
Swift <swiftshineexists@gmail.com>
2025-10-14 16:33:46 -0500

    link DvdLockedFileStream

--- configure.py
+++ configure.py
@@ -442,7 +442,7 @@ config.libs = [
             Object(Matching, "nw4r/ut/ut_IOStream.cpp"),
             Object(Matching, "nw4r/ut/ut_FileStream.cpp"),
             Object(Matching, "nw4r/ut/ut_DvdFileStream.cpp"),
-            Object(NonMatching, "nw4r/ut/ut_DvdLockedFileStream.cpp"),
+            Object(Matching, "nw4r/ut/ut_DvdLockedFileStream.cpp"),
             Object(NonMatching, "nw4r/ut/ut_LockedCache.cpp"),
         ],
     },
