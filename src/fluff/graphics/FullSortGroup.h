47c9d5444ebfa72b9d6f471bec2d70f61c2df9cb
Swift <swiftshineexists@gmail.com>
2025-12-12 17:36:51 -0600

    ScnObjGatherDiv

--- src/fluff/graphics/FullSortGroup.h
+++ src/fluff/graphics/FullSortGroup.h
@@ -20,4 +20,31 @@ public:
 
 ASSERT_SIZE(FullSortGroup, 0xF0);
 
+
+/// @note Perhaps they decided to use some other class instead, as most
+/// of the functionality of this class doesn't exist and instead
+/// consists of asserts.
+class ScnObjGatherDiv : nw4r::g3d::IScnObjGather {
+public:
+    inline ScnObjGatherDiv() { }
+
+    /* virtual methods */
+
+    /* nw4r::g3d::IScnObjGather */
+
+    /* 0x08 */ DECL_WEAK virtual ~ScnObjGatherDiv() override;
+    /* 0x0C */ virtual CullingStatus Add(nw4r::g3d::ScnObj* pObj, bool opa, bool xlu);
+    /* 0x10 */ virtual void Clear() override;
+    /* 0x14 */ virtual void ZSort() override;
+    /* 0x18 */ virtual void Sort() override;
+    /* 0x1C */ virtual void Sort(LessThanFunc pOpaFunc, LessThanFunc pXluFunc) override;
+    /* 0x20 */ virtual void DrawOpa(nw4r::g3d::ResMdlDrawMode* pForceMode) override;
+    /* 0x24 */ virtual void DrawXlu(nw4r::g3d::ResMdlDrawMode* pForceMode) override;
+
+    /* ScnObjGatherDiv */
+
+    /* 0x28 */ DECL_WEAK virtual int vf28();
+
+};
+
 #endif
