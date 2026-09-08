a6a9c4e24da7297674f026fa7af2d4ffbb06cc46
Swift <swiftshineexists@gmail.com>
2026-09-08 13:24:30 -0500

    Update LayoutObject.h

--- src/fluff/layout/LayoutObject.h
+++ src/fluff/layout/LayoutObject.h
@@ -1,10 +1,14 @@
 #ifndef FLUFF_LAYOUTOBJECT_H
 #define FLUFF_LAYOUTOBJECT_H
 
-#include "types.h"
+#include <types.h>
 #include <string>
+
+#include <nw4r/lyt/lyt_pane.h>
 #include <nw4r/lyt/lyt_drawInfo.h>
-#include "gfl/gflTask.h"
+
+#include <gfl/gflTask.h>
+
 #include "message/MessageTagProcessor.h"
 #include "layout/LayoutObjectRender.h"
 
@@ -24,10 +28,10 @@ public:
 
     /* 0x004 */ gfl::Task mGameTask;
     /* 0x01C */ gfl::Task mWorldMtxTask;
-    /* 0x034 */ s32 m_34;
-    /* 0x038 */ s32 m_38;
+    /* 0x034 */ nw4r::lyt::Pane* mPane;
+    /* 0x038 */ gfl::Scene* mScene;
     /* 0x03C */ nw4r::lyt::DrawInfo mDrawInfo;
-    /* 0x090 */ s32 m_90;
+    /* 0x090 */ u32 mFlags;
     /* 0x094 */ message::MessageTagProcessor mMessageTagProcessor;
     /* 0x0A0 */ std::string mArchivePath;
     /* 0x0AC */ std::string mBRLYTPath;
@@ -46,11 +50,11 @@ public:
     /* 0x0E4 */ s32 m_E4;
     /* 0x0E8 */ s32 m_E8;
     /* 0x0EC */ s32* m_EC;
-    /* 0x0F0 */ s32* m_F0[4];
+    /* 0x0F0 */ LayoutCallbackFunc<LayoutObject*, void(LayoutObject::*)()const>* mCallbacks[4];
     /* 0x100 */ u8 m_100[16];
     /* 0x110 */ u8 m_110[32];
     /* 0x130 */ s32 m_130;
-    /* 0x134 */ s32 m_134;
+    /* 0x134 */ u32 mLayoutFlags;
 };
 
 }
